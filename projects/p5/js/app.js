ca0bd0b42eff1cf85f028423d65d8ac04924ac94
M LOW <mlow991@users.noreply.github.com>
2016-04-23 21:04:16 -0700

    feat: Calculator logic for exceptions

--- projects/p5/js/app.js
+++ projects/p5/js/app.js
@@ -2,10 +2,30 @@ $(document).ready(function() {
 	$('#calc').click(function() {
 		var number = $('#num').val();
 		var range = [];
-		range[0] = Number($('#lbound').val());
-		range[1] = Number($('#ubound').val());
+		range[0] = $('#lbound').val();
+		range[1] = $('#ubound').val();
 		if(number == '') {
 			console.log("Please Enter a Number");
+			$('.num-warning').css('display', 'initial');
+		} else {
+			$('.num-warning').css('display', 'none');
+			number = Number(number);
+		}
+
+		if(range[0] == '' || range[1] == '') {
+			console.log("Please Enter a Valid Range");
+			$('.range-warning').css('display', 'initial');
+		} else {
+			$('.range-warning').css('display', 'none');
+			range[0] = Number(range[0]);
+			range[1] = Number(range[1]);
+		}
+		var diff = Math.abs(range[0] - range[1]);
+		if(diff == 0) {
+			console.log("Please Enter a Range Greater Than Zero");
+			$('.zero-range-warning').css('display', 'initial');
+		} else {
+			$('.zero-range-warning').css('display', 'none');
 		}
 	});
 	console.log('app loaded');
