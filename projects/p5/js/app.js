b398c94d45129c23758c6b770fe0eb8155486593
mlow991 <m.low91@gmail.com>
2016-04-24 16:26:15 -0700

    feat: Second variable added to calculator
    TODO: Cover all exceptions, such as when calculating with two variables
    and the user inputs a zero in the field.

--- projects/p5/js/app.js
+++ projects/p5/js/app.js
@@ -1,3 +1,33 @@
+var count = 0;
+var result;
+
+function calc(x, y, range) {
+	if(y == 0) {
+		var arr = [];
+		var total = 0;
+		for(i = range[0]; i < range[1] / x; i++) {
+			var val = i * x;
+			arr.push(val);
+			total += val;
+		}
+		result = total;
+		console.log(arr);
+		console.log(total);
+	} else {
+		var arr = [];
+		var total = 0;
+		for(i = range[0]; i < range[1]; i++) {
+			if(i % x == 0 || i % y ==0) {
+				arr.push(i);
+				total += i;
+			}
+		}
+		result = total;
+		console.log(arr);
+		console.log(total);
+	}
+}
+
 $(document).ready(function() {
 	$('#calc').click(function() {
 		var number = $('#num').val();
@@ -12,6 +42,18 @@ $(document).ready(function() {
 			$('.num-warning').css('display', 'none');
 			number = Number(number);
 			calculate++;
+		}
+
+		if(count > 0) {
+			var number2 = $('#num2').val();
+			if(number2 == '') {
+				console.log("Please Enter a Second Number");
+				$('.num-warning-two').css('display', 'initial');
+			} else {
+				$('.num-warning-two').css('display', 'none');
+				number2 = Number(number2);
+				calculate++;
+			}
 		}
 
 		var rangeCondition = 0;
@@ -36,21 +78,35 @@ $(document).ready(function() {
 			calculate++;
 		}
 
-		if(calculate == 3) {
-			range = range.sort(function(a, b) {
-				return a-b
-			});
-			console.log(range);
-			var arr = [];
-			var total = 0;
-			for(i = range[0]; i < range[1] / number; i++) {
-				var val = i * 3;
-				arr.push(val);
-				total += val;
-			}
-			console.log(arr);
-			console.log(total);
+		range = range.sort(function(a, b) {
+			return a-b
+		});
+		console.log(range);
 
+		if(calculate == 3) {
+			if(number != 0) {
+				calc(number, 0, range);
+			} else {
+				result = 0;
+			}
+		}
+
+		if(calculate == 4) {
+			if(number != 0 && number2 != 0) {
+				calc(number, number2, range);
+			} else {
+				result = 0;
+			}
+		}
+
+	});
+
+	$('#add').click(function() {
+		if(count <= 0) {
+			count++;
+			$('.num-warning').after('<br><br>Number 2: <input id="num2" type="number" name="Given Number"><span class="num-warning-two">Please Enter a Number</span>');
+		} else {
+			$('.num-amount').css('display', 'initial');
 		}
 	});
 	console.log('app loaded');
